c1cfec2340a091494cd6db7ccea984d20d4c6c97
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-01 18:00:25 +0200

    Add default profile.

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("i18nService")
-@Profile("ES")
-public class I18nSpanishService implements GreetingService {
+@Profile({"ES", "default"})
+public class I18nSpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
         return "Hola Mundo - ES";
